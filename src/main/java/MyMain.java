1729ffc5dcb012955c89d8850380501921dc5eb8
sthompson-athenian <sthompson@athenian.org>
2025-08-22 16:03:41 -0700

    Update Gradle to 7.6, Java to 17, and apply code formatting

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,30 +1,36 @@
 import java.util.Arrays;
 
 public class MyMain {
-    // Sorting:
-    // Implement the selection sort algorithm that we discussed in class
-    // to sort our code
-    // This algorithm returns the sorted array
-    public static int[] selectionSort(int[] arr) {
-        // REPLACE THIS WITH YOUR CODE
-        return arr;
-    }
+  // Sorting:
+  // Implement the selection sort algorithm that we discussed in class
+  // to sort our code
+  // This algorithm returns the sorted array
+  public static int[] selectionSort(int[] arr) {
+    // REPLACE THIS WITH YOUR CODE
+    return arr;
+  }
 
-    // Implement the insertion sort algorithm that we discussed in class
-    // to sort our code
-    // This algorithm returns the sorted array
-    public static int[] insertionSort(int[] arr) {
-        // REPLACE THIS WITH YOUR CODE
-        return arr;
-    }
+  // Implement the insertion sort algorithm that we discussed in class
+  // to sort our code
+  // This algorithm returns the sorted array
+  public static int[] insertionSort(int[] arr) {
+    // REPLACE THIS WITH YOUR CODE
+    return arr;
+  }
 
-    public static void main(String[] args) {
-        System.out.println("Selection Sort Tests");
-        System.out.println(Arrays.toString(selectionSort(new int[] {6, 1, 3, 8, 2, 9}))); // [1, 2, 3, 6, 8, 9]
-        System.out.println(Arrays.toString(selectionSort(new int[] {12, 5, 23, 74, 15, 86, 2}))); // [2, 5, 12, 15, 23, 74, 86]
+  public static void main(String[] args) {
+    System.out.println("Selection Sort Tests");
+    System.out.println(
+        Arrays.toString(selectionSort(new int[] {6, 1, 3, 8, 2, 9}))); // [1, 2, 3, 6, 8, 9]
+    System.out.println(
+        Arrays.toString(
+            selectionSort(new int[] {12, 5, 23, 74, 15, 86, 2}))); // [2, 5, 12, 15, 23, 74, 86]
 
-        System.out.println("Selection Sort Tests");
-        System.out.println(Arrays.toString(selectionSort(new int[] {6, 1, 3, 8, 2, 9}))); // [1, 2, 3, 6, 8, 9]
-        System.out.println(Arrays.toString(selectionSort(new int[] {12, 5, 23, 74, 15, 86, 2}))); // [2, 5, 12, 15, 23, 74, 86]
-    }
+    System.out.println("Selection Sort Tests");
+    System.out.println(
+        Arrays.toString(selectionSort(new int[] {6, 1, 3, 8, 2, 9}))); // [1, 2, 3, 6, 8, 9]
+    System.out.println(
+        Arrays.toString(
+            selectionSort(new int[] {12, 5, 23, 74, 15, 86, 2}))); // [2, 5, 12, 15, 23, 74, 86]
+  }
 }
